2c71cb75457042bc449bc90347bdc2e6f67e019e
Kishan <kishanp@thirdrocktechkno.com>
2022-05-17 12:29:14 +0530

    add analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,20 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Global site tag (gtag.js) - Google Analytics  */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-TVREW7P4M3"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-TVREW7P4M3');`,
+            }}
+          ></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('lYNOR8xowqvaWQJZ');`,
