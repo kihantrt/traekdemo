962acf37a8b9d471ffeab191d8aaffac9de43350
Kishan <kishanp@thirdrocktechkno.com>
2022-05-17 11:51:35 +0530

    add site content

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: ``,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <div id="notifications"></div>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
