32c2f79a35e286e64660875097e2f9e406e71c6f
Kishan <kishanp@thirdrocktechkno.com>
2022-05-17 11:52:17 +0530

    add leadfeeder script

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <script
             dangerouslySetInnerHTML={{
-              __html: ``,
+              __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('YEgkB8lPxbv8ep3Z');`,
             }}
           ></script>
         </Head>
